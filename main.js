09f3f72dcbcfe87c3cbac85e5f365dcbf96bfc38
Maka <makanimakasete@gmail.com>
2024-07-01 23:31:05 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
+//this makes sure that my document has been loaded 
 document.addEventListener('DOMContentLoaded', () => {
 
+    //json with main content
 const jsonContentData = [
     {"title": "Qué es HTML", "content": "HTML es..."},
-    {"title": "Qué es CSS", "content": "CSS es..."}
+    {"title": "Qué es CSS", "content": "CSS es..."},
+    {"title": "Qué es JS", "content": "JS es..."}
 ];
 
+//looks for side menu 
 let menu = document.getElementById('menu');
 
+//fills up side menu with the json titles
 jsonContentData.forEach(item => {
     const liItem = document.createElement('li');
-    liItem.content = item.title;
     liItem.innerHTML = item.title;
     menu.appendChild(liItem);
 });
-console.log("hello")
+
 })
